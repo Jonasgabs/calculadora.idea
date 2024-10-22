174250f00496973ea3d1d47999a2e243abc735b7
Jonas <jonas.sarmentof@gmail.com>
2024-10-22 18:33:24 -0300

    versao final

--- app.py
+++ app.py
@@ -4,34 +4,36 @@ class calculator:
         self.nome = nome
         while True:
             operador = self.menu()
-            if operador != "v":
+            if operador == "v":
+                print("fechando a calculadora")
+                break     
+            elif operador == "operador inválido":
+                print(operador)   
+            else:
                 self.get__numbers()
                 resultado = self.calculate(operador)
                 if resultado:
                     print(resultado)
-            else:
-                print("fechando a calculadora")
-                break
 
-        
 
     def get__numbers(self):
-        self.num1 = float(input("digite o primeiro operador:"))
-        self.num2 = float(input("digite o segundo operador:"))
+        self.num1 = float(input("digite o primeiro valor:"))
+        self.num2 = float(input("digite o segundo valor:"))
         return self.num1, self.num2
 
-    def menu(self):
 
+    def menu(self):
+            valid_operator = ["i","ii","iii","iv","v"]
             operador = input(f"""
-Informe o Operador:
+            Informe o Operador:
             (i) soma
             (ii)subtração
             (iii)multiplicação 
             (iv) divisão
             (v) Sair                                 
                                   
-:""")
-            return operador
+        :""")
+            return operador if operador in valid_operator else "operador inválido"
 
 
     def calculate(self,operador):
@@ -44,10 +46,12 @@ Informe o Operador:
             case "iii":
                 return self.num1 * self.num2
             case "iv":
+                if self.num2 ==0:
+                    return "Não é possível dividir por zero"
                 return self.num1 / self.num2
-            case _:
-                print("operador inválido")
-                return False               
+            
+                
+                              
             
 
 
