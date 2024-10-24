f03642bd45431fca9a3b20d365bb369ac7979b56
Jonas <jonas.sarmentof@gmail.com>
2024-10-24 00:07:52 -0300

    v1

--- app.py
+++ app.py
@@ -1,61 +1,57 @@
-class calculator:
-    
-    def __init__(self, nome):
+class Calculator:
+    historico = []
+
+    def __init__(self,nome):
         self.nome = nome
+
         while True:
             operador = self.menu()
-            if operador == "v":
-                print("fechando a calculadora")
-                break     
-            elif operador == "operador inválido":
-                print(operador)   
+            if operador == "operador inválido":
+                print("operador inválido, por favor insira o código correto: ")
+                continue
             else:
-                self.get__numbers()
-                resultado = self.calculate(operador)
-                if resultado:
-                    print(resultado)
+                num1,num2 = self.get_data()
+                result = self.calc(num1,num2)
+                print(result)
+                self.historico.append(result)
+                reset = input("Deseja continuar ? S ou N:  ").upper()
+
+                if reset == "S":
+                    continue
+                else:
+                    print(*self.historico)
+                    break
 
 
-    def get__numbers(self):
-        self.num1 = float(input("digite o primeiro valor:"))
-        self.num2 = float(input("digite o segundo valor:"))
-        return self.num1, self.num2
 
 
     def menu(self):
-            valid_operator = ["i","ii","iii","iv","v"]
-            operador = input(f"""
-            Informe o Operador:
-            (i) soma
-            (ii)subtração
-            (iii)multiplicação 
-            (iv) divisão
-            (v) Sair                                 
-                                  
-        :""")
-            return operador if operador in valid_operator else "operador inválido"
+        valid_op = ["i","ii", "iii","iv"]
+        self.operador = input(f'''Defina a operação:  (i) soma (ii)subtração (iii)multiplicação (iv) divisão:  ''')
+        return self.operador if self.operador in valid_op else  "operador inválido"
 
+    def get_data(self):
+        self.num1 = float(input("digite o numero 1:  "))
+        self.num2 = float(input("digite o numero 2:  "))
+        return self.num1, self.num2
 
-    def calculate(self,operador):
-        
-        match operador:
+    def calc(self, num1, num2):
+
+        match self.operador:
             case "i":
-                return self.num1 + self.num2
+                return f"{num1} + {num2} = {num1 + num2}\n"
             case "ii":
-                return self.num1 - self.num2
+                return f"{num1} - {num2} = {num1 - num2}\n"
             case "iii":
-                return self.num1 * self.num2
+                return f"{num1} x {num2} = {num1 *  num2}\n"
             case "iv":
-                if self.num2 ==0:
-                    return "Não é possível dividir por zero"
-                return self.num1 / self.num2
-            
-                
-                              
+                if num2 != 0:
+                    return f"{num1} / {num2} = {num1 / num2}\n"
+                else:
+                    return f"{num1} / {num2} = Não é possível dividir por zero\n"
             
 
 
 
 
-nome = calculator(nome =input("Digite seu nome: "))
-
+user = Calculator(input("digite o seu nome: "))
